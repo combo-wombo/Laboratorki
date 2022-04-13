b5f641e9985a36a7ff409be3d070526f5d20aad7
Andrey Komissarov <76626035+combo-wombo@users.noreply.github.com>
2022-04-13 13:25:41 +0300

    ksis 2 & 3

--- Labi/KSiS/2/source.asm
+++ Labi/KSiS/2/source.asm
@@ -1,84 +1,155 @@
-;statement: Текст содержит равное количество прописных латинских и прописных русских букв
-;     true: кждую стрчную латнскую бкву сответсвующе прописной буквой,а прписную–строчной
-;    false: Циклически  сдвинуть  текст  на K (константа)  позиций  влево  без  использования 
-;           дополнительной памяти, реализовав следующий алгоритм: перевернуть подмассив из первых K символов; 
-;           перевернуть оставшийся подмассив; перевернуть весь текст  
-;===========================================================================================================
-section .data
-  entermsg db 'Enter your message: '
-  enlen equ $ - entermsg
-  resultmsg db 0xa, 'Final result: '
-  reslen equ $ - resultmsg
-  
-  truemsg db 0xa, 'The statement is true! (jumping to func1)'
-  trlen equ $ - truemsg
-  
-  falsemsg db 0xa, 'The statement is false. (jumping to func2)'
-  fllen equ $ - falsemsg
-section .bss
-  input resb 100
-  input2 resb 3
-section .text
-  global _start
-	
-textWelcome:
-  mov edx, enlen
-  mov ecx, entermsg
-  mov ebx, 1
-  mov eax, 4
-  int 0x80
-  ret
-  
-textTrue:
-  mov edx, trlen
-  mov ecx, truemsg
-  mov ebx, 1
-  mov eax, 4
-  int 0x80
-  ret
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+//CP-1251 Кодировка:
+//65-90  -  ПРОПИСНЫЕ ЛАТИНСКИЕ буквы 
+//192-223 - ПРОПИСНЫЕ РУССКИЕ   буквы
+//97-122  - строчные  латинские буквы
+//224-255 - строчные  русские   буквы
 
-textFalse:
-  mov edx, fllen
-  mov ecx, falsemsg
-  mov ebx, 1
-  mov eax, 4
-  int 0x80
-  ret
-  
-textResult:
-  mov edx, reslen
-  mov ecx, resultmsg
-  mov ebx, 1
-  mov eax, 4
-  int 0x80
-  ret
-  
-input100:
-  mov eax, 3
-  mov ebx, 0
-  mov ecx, input
-  mov edx, 100
-  int 80h
-  ret
- 
-outInput:
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, input
-  mov edx, 101
-  int 80h
-  ret
-	
-exit:
-  mov ebx,0   ;process' exit code
-  mov eax,1   ;(sys_exit)
-  int 80h    ;kernel reads the eax register
 
-_start:
-  call textWelcome
-  call input100
-  call outInput
-  call textTrue
-  call textFalse
-  call textResult
-  call exit
+int condition(char* str, int len) {	//Это функция на проверку строки, для того чтобы знать, какое из двух условий использовать.
+	int checker = 0; _asm{	
+		mov eax, checker
+		mov esi, str //указатель на текст
+		mov ecx, len //счётчик цикла ecx будет равен длине строки
+		xor ebx, ebx //очищаем edx (счётчик прописных латинских букв)
+		xor edx, edx //очищаем ebx (счётчик прописных русских букв)
+		jmp start
+	//собственно проверка условия
+	start:	mov al, [esi]
+			cmp al, 65		//является ли символ буквой или знаком препинания?
+			jb nxt			//знак препинания - переход в nxt
+			jge latin		//буква - переход в latin
+
+	nxt :	inc esi			//переходим на следующий символ
+			loop start		//начинаем всё заново если ecx (счётчик цикла) не ноль
+			jmp check		//если мы прошли всю строку (ecx = 0), заканчиваем проверку
+		
+	latin :	cmp al, 90		//является ли символ латинским прописным?
+			jg rus1			//нет - переход в rus1 (начинаем проверку на русский символ)
+			jbe inceng		//да - переход в inceng (увеличим счётчик ebx)
+
+	rus1 :	cmp al, 192		//является ли символ русским прописным (1 часть)?
+			jge rus2		//возможно - переход в rus2 (заканчиваем проверку на русский символ)
+			jb nxt			//нет - переход в nxt
+
+	rus2 :  cmp al, 223		//является ли символ русским прописным (2 часть)?
+			jbe incrus		//да - переход в incrus (увеличим edx)
+			jg nxt			//нет - переход в nxt
+
+	inceng: inc ebx		//увеличиваем счетчик прописных латинских букв
+			jmp nxt		
+	incrus: inc edx		//увеличиваем счетчик прописных русских букв
+			jmp nxt
+	check :	cmp ebx, edx	//проверка на равенство прописных русских и латинских букв
+			je rule1		//если	  равны переход в rule1
+			jne rule2		//если не равны переход в rule2
+		
+	rule1 : mov eax, 1	//Checker = 1, будем выполнять первое правило
+			mov checker, eax
+			jmp ext
+	rule2 :	mov eax, 0	//Checker = 0, будем выполнять второе правило
+			mov checker, eax
+			jmp ext
+	ext : //выход
+	}	return checker;
+}
+
+char* invert(char* str, int K, int strOffset) {
+	_asm{
+	jmp start
+	revrse: mov bx, 2		//делим eax на 2 части
+			div bx			//чтобы поменять половину строки с другой
+			mov ecx, eax	//поместим в ecx значение eax
+	rev :	mov al, [edi]
+			movsb			//скопируем байт из edi в esi
+			dec esi			//уменьшим esi
+			mov[esi], al
+			dec esi			//уменьшим esi
+			dec ecx			//уменьшим ecx
+			cmp ecx, 0
+			jne rev			//если не равно 0 переход в rev
+			ret				//выход из подпрограммы
+    start :	mov ecx, str
+			mov eax, K		   //установим количество символов, которые нужно перевернуть
+			mov edx, strOffset //установим смещение
+			cmp edx, 0
+			jne m1			//если не равно переход в m1
+	m1 :	inc ecx			//сместим строку на 1 символ вправо
+			dec edx			//уменьшим счетчик смещения на 1
+			cmp edx, 0
+			jne m1			//если не равно переход в m1
+			jmp m2			//переход в m2
+	m2 :	mov edi, ecx	//установим указатель на элемент строки, с которого начать смещение 
+			mov esi, ecx	//установим указатель на элемент строки, с которого начать смещение
+			add esi, eax
+			dec esi
+			call revrse		//вызов смещения
+			jmp ext
+	ext :
+	}	return str;
+}
+
+char* firstRule(char* str, int len) {
+	_asm
+	{
+	jmp start
+	//Заменить  каждую  строчную  ЛАТИНСКУЮ  букву  соответствующей  прописной  буквой,  а прописную – строчной
+	start:	mov esi, str	//записываем указатель на текст в регистр esi
+			mov ecx, len	//устанавливаем счётчик цикла равным длине строки
+			jmp checkRule1
+	checkRule1 : 
+			mov al, [esi]
+			cmp al, 65		//входит ли буква в список латинских прописных (часть 1)?
+			jb nxt			//нет - если меньше переход в nxt
+			jge h2			// да -	если больше или равно переход в h2
+	nxt :	inc esi			//переход на след. символ
+			loop checkRule1
+			jmp ext
+	h2 :	cmp al, 90		//буква является прописной латинской (часть 2)?
+			jg h3			//нет - если больше переход в h3
+			jbe h4			//да - заменяем на строчную
+	h4 :	add al, 32		//замена прописной латинской на строчную букву
+			mov[esi], al
+			jmp nxt
+	h3 :	cmp al, 97		//проверка: входит ли буква в список латинских строчных?
+			jge h5			//если больше или равно переход в h5
+			jb nxt			//если меньше переход в h1
+	h5 :	cmp al, 122
+			jbe h6			//если меньше или равно переход в h6
+			jg nxt			//если больше переход в h1
+	h6 :	sub al, 32		//замена строчной латинской на прописную букву
+			mov[esi], al
+			jmp nxt
+	ext :
+	}	return str;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int maxLength = 100; int realLength;
+	char* mystring = new char[maxLength];
+	cout << "Enter a string: " << endl;
+	for (int i = 0; i < maxLength; i++) {
+		cin >> mystring[i];	if (mystring[i] == '.') {mystring[i] = NULL;break;}
+	}
+	realLength = strlen(mystring);
+
+	if (condition(mystring, realLength) == 1) {
+		cout << "First rule: \n";
+		cout << firstRule(mystring, realLength) << endl;
+	}
+	else {
+	cout << "Second rule: \n";
+	int K = rand() % (realLength - 1) + 1;
+	int secondPart = realLength - K;
+	invert(mystring, K, 0);
+	invert(mystring, secondPart, K);
+	cout << "Moving symbols (K = " << K << "):\n";
+	cout << invert(mystring, realLength, 0) << endl;
+	}
+
+	return 0;
+}
