af9bb83d40bb2b235df65e640cc2fb4c62b3402f
Andrey Komissarov <76626035+combo-wombo@users.noreply.github.com>
2023-04-19 02:57:40 +0300

    19.04.2023 (02:57)

--- Labi/СПП/4/2 пример/src/Lab10/src/main/java/lab10/Controller.java
+++
@@ -1,3 +0,0 @@
-package lab10;
-
-public class Controller {}
---
+++ Labi/СПП/4/spp.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 19 2023 г., 01:27
+-- Версия сервера: 10.4.25-MariaDB
+-- Версия PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `spp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `document` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `drivers`
+--
+
+INSERT INTO `drivers` (`id`, `name`, `document`) VALUES
+(1, 'Andrey E.', '123123'),
+(2, 'Maksim K.', '933356'),
+(3, 'Nikita S.', '183042');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `periods`
+--
+
+CREATE TABLE `periods` (
+  `id` int(11) NOT NULL,
+  `start` varchar(30) NOT NULL,
+  `end` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `periods`
+--
+
+INSERT INTO `periods` (`id`, `start`, `end`) VALUES
+(1, '05:39', '07:21'),
+(2, '06:00', '20:00'),
+(3, '13:00', '23:01');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `routes`
+--
+
+CREATE TABLE `routes` (
+  `id` int(11) NOT NULL,
+  `start` varchar(40) NOT NULL,
+  `end` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `routes`
+--
+
+INSERT INTO `routes` (`id`, `start`, `end`) VALUES
+(1, 'Minsk', 'Brest'),
+(2, 'Brest', 'Moskow'),
+(3, 'Brest', 'Washington D.C.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL,
+  `cost` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `cost`) VALUES
+(4, '100.49'),
+(5, '14.50'),
+(6, '4.99');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `transport`
+--
+
+CREATE TABLE `transport` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `number` varchar(40) NOT NULL,
+  `route_id` int(11) NOT NULL,
+  `period_id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  `driver_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `transport`
+--
+
+INSERT INTO `transport` (`id`, `name`, `number`, `route_id`, `period_id`, `ticket_id`, `driver_id`) VALUES
+(2, 'Троллейбус', '103', 2, 3, 6, 1),
+(3, 'Авиарейс', '412', 3, 3, 4, 3),
+(4, 'Маршрутка', '18', 1, 2, 5, 2);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `drivers`
+--
+ALTER TABLE `drivers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `periods`
+--
+ALTER TABLE `periods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `routes`
+--
+ALTER TABLE `routes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `transport`
+--
+ALTER TABLE `transport`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `periods`
+--
+ALTER TABLE `periods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `routes`
+--
+ALTER TABLE `routes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `transport`
+--
+ALTER TABLE `transport`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
